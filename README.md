6ea25a885857f96a3fd42376db87033ed0eb9469
himanshu-dutta <meet.himanshu.dutta@gmail.com>
2021-01-01 19:27:00 +0530

    Initiated with data scraping utilities.

---
+++ README.md
@@ -0,0 +1,5 @@
+# ML Utils
+
+This repo is an in progress collection of different Machine Learning and Deep Learning utilities in Python, related to data collection, data preprocessing, model builing, training, deployment, etc.
+
+With every project, I intend to add functionalities that are fairly genralized to be put as a boilerplate.
---
+++ data/scraper.py
@@ -0,0 +1,60 @@
+import os
+import math
+import logging
+from tqdm import *
+import urllib.request
+from typing import Union, List
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def init_driver(fetcher):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    tab = webdriver.Chrome(ChromeDriverManager(
+        log_level=logging.WARNING).install(), options=options)
+
+    def fetcher_with_tab(*args, **kwargs):
+        return fetcher(*args, **kwargs, driver=tab)
+
+    return fetcher_with_tab
+
+
+@init_driver
+def google_image_scraper(search_term: str, scroll_down_by: int = 10, driver: webdriver.chrome.webdriver.WebDriver = None) -> list:
+    base_url = "https://www.google.com/imghp?hl=en"
+    driver.get(base_url)
+    search_form = driver.find_element_by_name("q")
+    search_form.send_keys(search_term)
+    search_form.submit()
+    for _ in tqdm(range(scroll_down_by)):
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+    urls = driver.execute_script(
+        "return Array.from(document.querySelectorAll(\'.rg_i\')).map(el=> el.hasAttribute(\'data-src\')?el.getAttribute(\'data-src\'):el.getAttribute(\'data-iurl\'));")
+
+    while None in urls:
+        urls.remove(None)
+
+    return urls
+
+
+def download_images_from_url(urls: Union[str, List[str]], image_tag: str, format: str = 'png',  save_location: str = ".") -> None:
+    dir = os.path.join(save_location, image_tag)
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+    if isinstance(urls, str):
+        urllib.request.urlretrieve(urls, os.path.join(
+            dir, "0001" + "." + format))
+        print(f"Downloaded 1 image in {dir}.")
+
+    elif isinstance(urls, list):
+        pad = math.ceil(math.log10(len(urls)))
+        for idx in tqdm(range(len(urls))):
+            urllib.request.urlretrieve(urls[idx], os.path.join(
+                dir, str(idx+1).zfill(pad) + "." + format))
+        print(f"Downloaded {len(urls)} images in {dir}.")
+
+    else:
+        raise Exception("Invalid argument type for \"urls\"")
